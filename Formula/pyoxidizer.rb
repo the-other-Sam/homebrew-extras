aa633cdbf00a970e79527d8ce0232db3e396919f
Sam <58614277+the-other-Sam@users.noreply.github.com>
2019-12-30 22:10:10 -0600

    Update pyoxidizer.rb

--- Formula/pyoxidizer.rb
+++ Formula/pyoxidizer.rb
@@ -4,6 +4,10 @@ class Pyoxidizer < Formula
   url "https://github.com/indygreg/PyOxidizer/archive/v0.4.0.tar.gz"
   sha256 "73f948c3b6e810f2cc1d241bab85bfc804166780e73f2935aed837d2644dbe11"
   head "https://github.com/indygreg/PyOxidizer.git", :using => :git
+  bottle do
+    cellar :any
+    sha256 "522508270f20a8e06dd31d969f861a67a0dcafec026dc55cfddb82b2b7490b37" => :catalina
+  end
   
   depends_on "rust" => :build
 
