a6cd0aace1c68cc1a92227529b20ddb9c044e473
Sam <58614277+the-other-Sam@users.noreply.github.com>
2019-12-30 19:59:37 -0600

    Update pyoxidizer.rb

--- Formula/pyoxidizer.rb
+++ Formula/pyoxidizer.rb
@@ -8,20 +8,15 @@ class Pyoxidizer < Formula
   #  cellar :any
     # sha256 "64fd79987eb5276eef4c58f0148244d71f2fa05a364c69030fdf60d3b259f689" => :catalina
   # end
-  # @name="pyoxidizer"
-  # version "0.4.0"
-  # sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-
+  
   depends_on "rust" => :build
 
   def install
-    # system "cargo", "install", "--locked", "--root", prefix, "--path", "."
-    # system "cargo", "install", "--root", "#{prefix}", "--version", "#{version}", "#{name}"
     system "cargo", "install", "--root", prefix, "--version", version, name
   end
 
   test do
     output = shell_output(bin/"pyoxidizer --version")
-    assert_match "pyoxidizer version #{version} (HEAD)", output
+    assert_match "pyoxidizer version version (HEAD)", output
   end
 end
