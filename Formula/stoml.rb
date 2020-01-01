efbd7e1d1f90768769f7ec4f01128a776165b974
Sam <58614277+the-other-Sam@users.noreply.github.com>
2019-12-31 19:04:09 -0600

    Update stoml.rb

--- Formula/stoml.rb
+++ Formula/stoml.rb
@@ -3,7 +3,13 @@ class Stoml < Formula
   homepage "https://github.com/freshautomations/stoml"
   url "https://github.com/freshautomations/stoml/archive/v0.4.0.tar.gz"
   sha256 "5b41845021181cf2e2204f9e30213d002ff2bc4c299a2b4edad1a9910e429546"
-
+  head "https://github.com/freshautomations/stoml.git", :using => :git
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://dl.bintray.com/the-other-sam/bottles-homebrew-extras"
+    sha256 "4997b8f58051174113ed313801ec0c31ed3ad1f0f741c0f5196974e0fb167d6a" => :catalina
+    sha256 "35fd506a079371eb992ca54a393ad14aaf86ecb96cfc6c31346c6f03104a6837" => :x86_64_linux
+  end
   depends_on "go" => :build
 
   def install
